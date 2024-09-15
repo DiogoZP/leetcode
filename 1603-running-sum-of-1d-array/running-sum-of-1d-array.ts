0c657b3427f3c828f6abf4b23baf2f0db681b35f
Diogo Zago <83012356+DiogoZP@users.noreply.github.com>
2024-09-15 11:57:55 -0300

    Time: 55 ms (88.82%) | Memory: 51.8 MB (95.62%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
@@ -1,9 +1,7 @@
 function runningSum(nums: number[]): number[] {
-    let result: number[] = [];
-    result.push(nums[0])
     for(let i = 1; i < nums.length; i++)
     {
-            result.push(result[i-1] + nums[i]);
+        nums[i] = nums[i-1] + nums[i];
     }
-    return result;
+    return nums;
 };
